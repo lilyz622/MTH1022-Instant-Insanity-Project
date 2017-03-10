a2cdafa5b657d954a44d8a0ad3b4184bde8cd915
Nick <nickbrandt2012@aol.com>
2017-03-09 19:02:24 -0500

    first commit nick

---
+++ src/Cube.java
@@ -0,0 +1,25 @@
+public class Cube {
+	
+private Pair LR;
+private Pair UD;
+private Pair FB;
+
+public Cube(Pair a, Pair b, Pair c){
+	LR = a;
+	UD = b;
+	FB = c;
+}
+
+public Pair getLeftRight(){
+	return LR;
+}
+
+public Pair getUpDown(){
+	return UD;
+}
+
+public Pair getFrontBack(){
+	return FB;
+} 
+
+}
---
+++ src/Pair.java
@@ -0,0 +1,20 @@
+public class Pair {
+	
+private char color1;
+private char color2;
+
+public Pair (char a, char b)
+{
+	color1 = a;
+	color2 = b;
+}
+
+public char getColor1(){
+	return color1;
+}
+
+public char getColor2(){
+	return color2;
+}
+
+}
