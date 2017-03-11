24fff6664b96f4e767ab39eb7b098763adb5f123
lilyz622 <lily.zhang622@yahoo.com>
2017-03-11 13:18:37 -0500

    changes to Pair.java

--- src/Pair.java
+++ src/Pair.java
@@ -1,4 +1,36 @@
+public class Pair 
+{
+	char col1;
+	char col2;
 
-public class Pair {
+	
+	public Pair(char one, char two)
+	{	
+		col1 = one;
+		col2 = two;
+	}
+	
+	
+	public char getColor1()
+	{
+		return col1;
+	}
 
+
+	public char getColor2()
+	{
+		return col2;
+	}
+
+
+	public String getBothColors()
+	{
+		return col1+col2;
+	}
+
+	
+	public String getColorsReverse()
+	{	
+		return col2+col1;
+	}
 }
