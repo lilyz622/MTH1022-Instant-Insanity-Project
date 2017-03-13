e80a6672399fb5b642723f0998f2a8fef12c3a31
Nick Brandt <nickbrandt2012@aol.com>
2017-03-13 19:49:52 -0400

    Update Pair.java

--- src/Pair.java
+++ src/Pair.java
@@ -22,17 +22,6 @@ public class Pair
 		return color2;
 	}
 
-	/* // Not sure if we need the methods below
-	public String getBothColors()
-	{
-		return color1+color2;
-	}
-
-	public String getColorsReverse()
-	{	
-		return color2+color1;
-	} */
-
 	public String toString()
 	{
 		return " " +color1+ "-" +color2+ " ";
